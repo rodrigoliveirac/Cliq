543390b18b29ac470bc573a015a3961b079f6fb0
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-03-08 08:23:43 -0300

    implementing the list of clients by filter and the logic of search

--- app/src/main/java/com/rodcollab/cliq/collections/bookings/form/BookingFormFragment.kt
+++ app/src/main/java/com/rodcollab/cliq/collections/bookings/form/BookingFormFragment.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rodcollab.cliq.collections.clients.domain.GetClientsUseCaseImpl
+import com.rodcollab.cliq.collections.clients.form.SearchClientListAdapter
+import com.rodcollab.cliq.core.Utils
 import com.rodcollab.cliq.core.repository.BookingRepositoryImpl
+import com.rodcollab.cliq.core.repository.ClientRepositoryImpl
 import com.rodcollab.cliq.databinding.FragmentBookingFormBinding
 
 class BookingFormFragment : Fragment() {
@@ -15,10 +21,22 @@ class BookingFormFragment : Fragment() {
     private var _binding: FragmentBookingFormBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var adapter: SearchClientListAdapter
+
     private val viewModel: BookingFormViewModel by activityViewModels {
 
+        val clientRepository = ClientRepositoryImpl
         val bookingsRepository = BookingRepositoryImpl
-        BookingFormViewModel.Factory(bookingsRepository)
+
+        val getClientsUseCase = GetClientsUseCaseImpl(clientRepository)
+        val onQueryTextChangeUseCase = OnQueryTextChangeUseCaseImpl(getClientsUseCase)
+
+        BookingFormViewModel.Factory(onQueryTextChangeUseCase, bookingsRepository)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        adapter = SearchClientListAdapter()
     }
 
     override fun onCreateView(
@@ -33,6 +51,31 @@ class BookingFormFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.searchViewRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.searchViewRecyclerView.adapter = adapter
+
+        val divider = Utils(requireContext()).addingDividerDecoration()
+
+        binding.searchViewRecyclerView.addItemDecoration(divider)
+
+        binding.searchViewClients.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                viewModel.onQueryTextChange(newText.toString())
+
+                viewModel.stateOnceAndStream().observe(viewLifecycleOwner) {
+                    adapter.submitList(it.clientList)
+                }
+
+                return false
+            }
+
+        })
         saveNewBooking()
     }
 
@@ -47,5 +90,4 @@ class BookingFormFragment : Fragment() {
             findNavController().navigateUp()
         }
     }
-
 }
--- app/src/main/java/com/rodcollab/cliq/collections/bookings/form/BookingFormViewModel.kt
+++ app/src/main/java/com/rodcollab/cliq/collections/bookings/form/BookingFormViewModel.kt
@@ -1,14 +1,31 @@
 package com.rodcollab.cliq.collections.bookings.form
 
 import androidx.lifecycle.*
+import com.rodcollab.cliq.collections.clients.model.ClientItem
 import com.rodcollab.cliq.core.repository.BookingRepository
-
+import kotlinx.coroutines.launch
 
 class BookingFormViewModel(
+    private val onQueryTextChangeUseCase : OnQueryTextChangeUseCase,
     private val bookingRepository: BookingRepository
 ) : ViewModel() {
 
+    private val uiState: MutableLiveData<UiState> by lazy {
+        MutableLiveData<UiState>(UiState(clientList = emptyList()))
+    }
 
+    fun stateOnceAndStream(): LiveData<UiState> {
+        return uiState
+    }
+
+    fun onQueryTextChange(query: String) {
+
+        viewModelScope.launch {
+            uiState.postValue(UiState(onQueryTextChangeUseCase(query)))
+        }
+    }
+
+    data class UiState(val clientList: List<ClientItem>)
 
     fun addBooking(
         time: String,
@@ -20,10 +37,11 @@ class BookingFormViewModel(
 
     @Suppress("UNCHECKED_CAST")
     class Factory(
+        private val onQueryTextChangeUseCase: OnQueryTextChangeUseCase,
         private val bookingRepository: BookingRepository
     ) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return BookingFormViewModel(bookingRepository) as T
+            return BookingFormViewModel(onQueryTextChangeUseCase,bookingRepository) as T
         }
     }
 }
