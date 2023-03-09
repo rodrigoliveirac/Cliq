adb5e15faf6c8362c2e0d2d066c5e5b84c1aa174
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-03-09 17:33:00 -0300

    handle when the client item was clicked and another values

--- app/src/main/java/com/rodcollab/cliq/collections/bookings/form/BookingFormViewModel.kt
+++ app/src/main/java/com/rodcollab/cliq/collections/bookings/form/BookingFormViewModel.kt
@@ -10,6 +10,12 @@ class BookingFormViewModel(
     private val bookingRepository: BookingRepository
 ) : ViewModel() {
 
+    private val _clientSelected: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>(false)
+    }
+    val clientSelected: LiveData<Boolean> = _clientSelected
+
+
     private val uiState: MutableLiveData<UiState> by lazy {
         MutableLiveData<UiState>(UiState(clientList = emptyList()))
     }
@@ -34,13 +40,43 @@ class BookingFormViewModel(
         bookingRepository.add(clientName, bookedDate, time)
     }
 
+    private val name: MutableLiveData<String> by lazy {
+        MutableLiveData<String>("")
+    }
+    val liveName: LiveData<String> = name
+
+    fun bookingSaved(bookingSaved: Boolean) {
+        _clientSelected.value.let {
+            _clientSelected.value = bookingSaved
+        }
+    }
+
+    fun onItemClicked(nameSelected: String) {
+        name.value?.let {
+            name.value = nameSelected
+        }
+        _clientSelected.value?.let {
+            _clientSelected.value = true
+        }
+    }
+
+    private val setValueDate: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val getValueDate: LiveData<String> = setValueDate
+
+    fun saveValueDate(headerText: String?) {
+        setValueDate.value = headerText
+    }
+
     @Suppress("UNCHECKED_CAST")
     class Factory(
         private val onQueryTextChangeUseCase: OnQueryTextChangeUseCase,
         private val bookingRepository: BookingRepository
     ) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
-            return BookingFormViewModel(onQueryTextChangeUseCase,bookingRepository) as T
+            return BookingFormViewModel(onQueryTextChangeUseCase, bookingRepository) as T
         }
     }
 }
