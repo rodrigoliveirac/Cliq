f0a57dd9cc5a973d8a79ccf40348b7f28856a260
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-03-15 11:04:17 -0300

    get the date from material picker and convert to yyyy-MM-dd Locale.US

--- app/src/main/java/com/rodcollab/cliq/core/repository/BookingRepositoryImpl.kt
+++ app/src/main/java/com/rodcollab/cliq/core/repository/BookingRepositoryImpl.kt
@@ -20,4 +20,40 @@ object BookingRepositoryImpl : BookingRepository {
         )
     }
 
+    private fun formattedDate(bookedDate: String) : String {
+       return when(Locale.getDefault().language) {
+            "pt" -> BrToUs.format(bookedDate)
+             else -> getDefaultUS(bookedDate)
+        }
+
+
+    }
+    private fun getDefaultUS(bookedDate: String): String {
+        val inputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+        val outputDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+        val date = inputDateFormat.parse(bookedDate)
+        return outputDateFormat.format(date!!)
+    }
+}
+
+object BrToUs {
+    fun format(bookedDate: String): String {
+        val day: String
+        val month: String
+        val year: String
+
+        val dateSlice = bookedDate.split(" ").slice(0..4 step 2)
+
+        day = dateSlice[0]
+        month = dateSlice[1].slice(0..2)
+        year = dateSlice[2]
+
+        val inputDateString = "$month $day, $year"
+        val inputDateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.US)
+        val outputDateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+
+        val date = inputDateFormat.parse(inputDateString)
+
+        return outputDateFormat.format(date!!)
+    }
 }
