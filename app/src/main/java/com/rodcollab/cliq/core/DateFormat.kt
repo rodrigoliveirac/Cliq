26d505f59a43212df0a5def839dbf592f86fa42c
Rodrigo Cavalcante <rodrigocavalcante.pro@gmail.com>
2023-03-20 12:01:47 -0300

    Creating a object to delegate format functions

---
+++ app/src/main/java/com/rodcollab/cliq/core/DateFormat.kt
@@ -0,0 +1,34 @@
+package com.rodcollab.cliq.core
+
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+
+object DateFormat {
+
+    fun formatDate(dateSelected: Long): String {
+        val date = getDate(dateSelected)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault())
+        return LocalDate.parse(formatter.format(date.toInstant()).toString(), formatter).plusDays(1)
+            .toString()
+    }
+
+     fun localDateToString(localDate: LocalDate): String {
+        val formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy", Locale.getDefault())
+        return formatter.format(localDate)
+    }
+
+    fun toLocalDate(datePicked: Long): LocalDate {
+        val date = getDate(datePicked)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault())
+        return LocalDate.parse(formatter.format(date.toInstant()).toString(), formatter).plusDays(1)
+    }
+
+    private fun getDate(dateSelected: Long): Date {
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = dateSelected
+        return calendar.time
+    }
+}
